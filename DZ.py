997f8b27a85814072a10d6f274d2a2aca3abf906
Rary1337 <chelyaeva2005@gmail.com>
2024-10-27 12:12:37 +0300

    Add files via upload

---
+++ DZ.py
@@ -0,0 +1,15 @@
+print(1223)
+# 1st program задача1
+print(9**0.5*5)
+# 2nd program
+print(99.9>99.8 and 1000!=1000.1)
+# 3d program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+#4th program
+print("123.456")
+print (float("123.456"))
+print (float("123.456")*10)
+print(float("123.456")*10)
+print(int(float("123.456")*10)%10)
