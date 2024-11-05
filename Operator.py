fece0f4c1594ddbf69144049f9b330185237956b
Rary1337 <sirtregubov@yandex.ru>
2024-11-05 20:51:48 +0300

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,14 @@
+first = int(input('Введите певрое число:'))
+second = int(input('Введите второе число:'))
+third = int(input('Введите третье число:'))
+if first == second == third:
+    print(3)
+elif first == third:
+    print(2)
+elif first == second:
+    print(2)
+elif third == second:
+    print(2)
+else:
+    print(0)
+
